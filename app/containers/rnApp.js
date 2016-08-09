d3086603c2fb400b8b89635a7e0a37da934d4355
Ned Rockson <nedrocks@gmail.com>
2016-08-09 14:44:04 -0700

    Need to get stream importing.

--- app/containers/rnApp.js
+++ app/containers/rnApp.js
@@ -4,15 +4,23 @@ import React, { Component } from 'react';
 
 import Leaderboard from '../components/Leaderboard'
 
+import { Text, View } from 'react-native';
+
+import { connection } from '../globals';
+
 class RNApp extends Component {
 
   constructor(props: Object) {
     super(props);
+
+    connection.connect().then((ob) => {
+      console.log('done');
+    })
   }
 
   render: Function = () => {
     return (
-      <Leaderboard />
+      <View><Text>Hello world</Text></View>
     )
   }
 }
--- app/globals.js
+++ app/globals.js
@@ -1,21 +1,63 @@
 /* @flow */
 
-const sharedb = require('sharedb/lib/client');
+// const sharedb = require('sharedb/lib/client');
+//
+// class Connection {
+//   connection: any;
+//
+//   constructor(websocketLoc: string) {
+//     this.connection = new sharedb.Connection(new WebSocket(websocketLoc));
+//   }
+//
+//   createQuery: Function = () => {
+//     return this.connection.createSubscribeQuery('players', {$sort: {score: -1}});
+//   }
+// }
+//
+// const connection = new Connection('ws://10.1.20.219:8080');
+//
+// export {
+//   connection,
+// }
+
+//const racerBrowserChannel = require('racer-browserchannel/lib/browserStandalone');
+const racer = require('racer');
 
 class Connection {
   connection: any;
 
-  constructor(websocketLoc: string) {
-    this.connection = new sharedb.Connection(new WebSocket(websocketLoc));
+  domain: string;
+  port: number;
+  path: string;
+
+  model: ?Object;
+
+  constructor(domain: string, port: number, path: string) {
+    this.domain = domain;
+    this.port = port;
+    this.path = path;
+    this.model = null;
   }
 
-  createQuery: Function = () => {
-    return this.connection.createSubscribeQuery('players', {$sort: {score: -1}});
+  connect: Function = (): Promise<Object> => {
+    const elem = `http://${this.domain}:${this.port}/${this.path}.json`;
+    return fetch(elem)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        responseJson.browserChannel = {
+          base: `http://${this.domain}:${this.port}/${this.path}`,
+        }
+        this.model = racer.createModel(responseJson);
+        this.model.at('_page.room');
+        return this.model;
+      }).catch((error) => {
+        console.log(error);
+      });
   }
 }
 
-const connection = new Connection('ws://10.1.20.219:8080');
+const connection = new Connection('10.1.20.219', 3000, 'home');
 
 export {
-  connection,
-}
+  connection
+};
